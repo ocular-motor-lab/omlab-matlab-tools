4f8120ad25de0703a6f1b2b47dd42878ba7db782
Jorge Otero-Millan <jom@berkeley.edu>
2024-03-09 17:38:02 -0800

    Added parabolic attractor

--- ConicTests.m
+++ ConicTests.m
@@ -27,21 +27,21 @@ X = [x1(:) x2(:) ones(size(x1(:)))];
 
 %circle
 
-A = 1;
-B = 0;
-C = 1;
-D = -2;
-E = -2;
-F = 1;
+% A = 1;
+% B = 0;
+% C = 1;
+% D = -2;
+% E = -2;
+% F = 1;
 
 % % line
 % % 
-% A = 0;
-% B = 0;
-% C = 0;
-% D = 1;
-% E = 1;
-% F = -2;
+A = 0;
+B = 0;
+C = 0;
+D = 1;
+E = 1;
+F = -2;
 
 % 
 % % Ellipse
@@ -66,14 +66,14 @@ F = 1;
 
 % % parabola
 
-% A = 0;
-% B = 0;
-% C = 1;
-% D = -4;
-% E = 0;
-% F = 0;
+A = 0;
+B = -1;
+C = 1;
+D = -1;
+E = -1;
+F = 1;
 
-Aq = [A B/2 D/2; B/2 C E/2; D/2 E/2 F ];
+Aq = [A B/2 D/2; B/2 C E/2; D/2 E/2 F ]     ;
 
 y = zeros(size(x1(:)));
 for i=1:height(X)
@@ -94,14 +94,20 @@ quiver(x1,x2,-reshape(g(:,1),size(x1)), -reshape(g(:,2),size(x1)))
 set(gca,'PlotBoxAspectRatio',[1 1 1])
 
 d = zeros(height(x1(:)),3);
+c = zeros(height(x1(:)),3);
 for i=1:height(X)
     d(i,:) = [0 -1 0;1 0 0; 0 0 0]*Aq*X(i,:)';
+    c(i,:) = Aq*X(i,:)';
 end
 
 subplot(1,2,2)
 contour(x1,x2,reshape(log(.1+y),size(x1)),40)
 hold
-% quiver(x1,x2,-reshape(d(:,1),size(x1)), -reshape(d(:,2),size(x1)))
-quiver(x1,x2,-reshape(g(:,1),size(x1)), -reshape(g(:,2),size(x1)),'r')
+ quiver(x1,x2,-reshape(d(:,1),size(x1)), -reshape(d(:,2),size(x1)))
+%quiver(x1,x2,-reshape(g(:,1),size(x1)), -reshape(g(:,2),size(x1)),'r')
 
 set(gca,'PlotBoxAspectRatio',[1 1 1])
+
+figure
+% plot3(c(:,1),c(:,2),c(:,3),'o')
+quiver(x1,x2,-reshape(c(:,1),size(x1)), -reshape(c(:,2),size(x1)),'r')
--- RingLinearAttractorDemo.m
+++ RingLinearAttractorDemo.m
@@ -9,7 +9,7 @@ dt = 0.001;
 trange = (tstart:dt:tend)';
 
 % initial conditions of the attractor
-e0 = [0 0]';
+y0 = [0 0]';
 
 % input velocity
 x = zeros(size(trange));
@@ -18,12 +18,7 @@ x(trange > 3 & trange <4) = 20;
 x(trange > 5 & trange <9) = -10;
 x = x+ randn(size(x))*.1;
 
-e0 = [0 0]';
-
 % ring attractor
-params.Wi = [0 -1 1; 1 0 -1]; 
-% the first two columns act as a rotation matrix, the last column controls
-% the center (should be minus the sum of the other numbers) 
 
 A = 1;
 B = 0;
@@ -31,44 +26,67 @@ C = 1;
 D = -2;
 E = -2;
 F = 1;
-params.Wr  = [A B/2 D/2; B/2 C E/2; D/2 E/2 F ];
+params.Aq  = [A B/2 D/2; B/2 C E/2; D/2 E/2 F ];
+params.FP = [1 1] - [cosd(45) sind(45)];
 
-% conic representation of the attractor
 
-[t, e] = ode45(@(t,y)ode2DAttractor(t,y,trange,x,params), trange, e0);
+[t, e] = ode45(@(t,y)ode2DAttractor(t,y,trange,x,params), trange, y0);
 
 figure
-subplot(2,3,1);
+subplot(3,3,1);
 plot(e(:,1),e(:,2)); set(gca,'xlim',[-1 3],'ylim',[-1 3])
 xlabel( 'Internal unit 1'), ylabel( 'Internal unit 2')
 title('Ring attractor')
 set(gca,'PlotBoxAspectRatio',[1 1 1])
-subplot(2,3,[2 3]);
+subplot(3,3,[2 3]);
 plot(t,[x e]);
 legend({'Input velocity', 'Internal unit 1', 'Internal unit 2'})
 xlabel('Time')
 
 
-% line attractor
+% parabollic attractor
+
+A = 0;
+B = -1;
+C = 1;
+D = -1;
+E = -1;
+F = 1;
+params.Aq = [A B/2 D/2; B/2 C E/2; D/2 E/2 F ];
+params.FP = -[0.5 0.5];
+
+[t, e] = ode45(@(t,y)ode2DAttractor(t,y,trange,x/10,params), trange, y0); % velocity made slower to avoid numerical problems
+
+subplot(3,3,4);
+plot(e(:,1),e(:,2)); set(gca,'xlim',[-1 3],'ylim',[-1 3])
+xlabel( 'Internal unit 1')
+ylabel( 'Internal unit 2')
+title('Parabollic attractor')
+set(gca,'PlotBoxAspectRatio',[1 1 1])
+subplot(3,3,[5 6]);
+plot(t,[x e]);
+legend({'Input velocity', 'Internal unit 1', 'Internal unit 2'})
+xlabel('Time')
+
+% % line attractor
 A = 0;
 B = 0;
 C = 0;
 D = 1;
 E = 1;
 F = -(D+E);
-params.Wr  =  [A B/2 D/2; B/2 C E/2; D/2 E/2 F ];
+params.Aq  =  [A B/2 D/2; B/2 C E/2; D/2 E/2 F ];
+params.FP = [1 1];
 
-params.Wi = [0 0 .1; 0 0 -.1];
+[t, e] = odeSimple(@(t,y)ode2DAttractor(t,y,trange,x,params), trange, y0);
 
-[t, e] = ode45(@(t,y)ode2DAttractor(t,y,trange,x,params), trange, e0);
-
-subplot(2,3,4);
+subplot(3,3,7);
 plot(e(:,1),e(:,2)); set(gca,'xlim',[-1 3],'ylim',[-1 3])
 xlabel( 'Internal unit 1')
 ylabel( 'Internal unit 2')
 title('Line attractor')
 set(gca,'PlotBoxAspectRatio',[1 1 1])
-subplot(2,3,[5 6]);
+subplot(3,3,[8 9]);
 plot(t,[x e]);
 legend({'Input velocity', 'Internal unit 1', 'Internal unit 2'})
 xlabel('Time')
@@ -77,22 +95,65 @@ xlabel('Time')
 function yd = ode2DAttractor(t,y,xt,x,params)
     x = interp1(xt,x,t); % Interpolate the data set (gt,g) at time t
     
-    yh = [y;1]; % make it homogeneous
+    y = [y;1]; % make it homogeneous
 
     % x is the input
-    % y is the state
+    % y is the state/output of the atractor
 
+    W = params.Aq;
+    Ortho1 = [0 -1 0;1 0 0; 0 0 0]; %rotates Wy to get the orthogonal direction along the attractor
+    Gleak = 0.2; % leak speed
+
+    fp = params.FP;
+    leak = y(2)*fp(1)-y(1)*fp(2);
+    
+    yd = (x-Gleak*leak)*Ortho1*W*y   -   (4*y'*W*y)*W*y;
+
+    yd = yd(1:2);
+    
     % the first term is the movement along the attractor. It can be either
     % driven by the input or by a leak towards a point in the attractor.
+
+    % right now the leak component is not ideal and does not extend to 3D
+    % it is sort of a cross product between the current position along the
+    % attractor and the set point. So you get the velocity that you need to
+    % drift by depending on how far you are. In this case I am assuming
+    % the set point is along the diagonal. But that is something that
+    % should be done via weights that need to learned too. 
 
     % the second term is the one that attracts to the conic curve
     % which can be a line, a circle or whatever. 
 
+    % the first term needs to learn the cross product or quaternion product
+    % (when we move to more dimensions)
+    % for more dimensions we need to calculate the cross product (somehow
+    % corrected by manifold shape) between the fixeed point and the current
+    % point. That will give the leak velocity which can be added in all
+    % components to x. Then x needs to be a applied along the orthogonal
+    % dimensions to the Aq*yb which is a vector towards the manifold. So
+    % those orthogonal components are the components along the manifold. 
 
-    yd = (x-0.2*(-yh(1)+y(2)))*[0 -1 0;1 0 0; 0 0 0]*params.Wr*yh- (4*yh'*params.Wr*yh*params.Wr*yh);
+    % I stil think there may be a way to combine the two components with a
+    % single tensor. The tensor will define how much to movement a long
+    % each of the components. 1 towards the attractor and n-1 along the
+    % attractor + 1 along the input regarless of attractor
 
-    yd = yd(1:2);
-    
 end
 
+
+function [t, yout] = odeSimple(fun, trange, y0)
+if ( length(trange) == 2 )
+    trange = linspace(trange(1),trange(2),1000);
+end
+yout= zeros(length(y0),length(trange));
+t = trange;
+
+yout(:,1) = y0;
+for i=2:length(t)
+    yout(:,i) = yout(:,i-1) + fun(t(i), yout(:,i-1))*(t(i)-t(i-1)) ;
+end
+
+yout = yout';
+
+end
 
