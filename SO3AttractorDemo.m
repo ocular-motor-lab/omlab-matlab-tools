664530669563d4af8232c6bd1e1d8d231864b47c
Jorge Otero-Millan <jom@berkeley.edu>
2024-03-10 22:30:25 -0700

    Update SO3AttractorDemo.m

--- SO3AttractorDemo.m
+++ SO3AttractorDemo.m
@@ -1,4 +1,7 @@
-%% attractor simulation
+%% SO3 attractor simulation
+%% Jorge Otero-Millan 3/10/2024
+
+
 clear params;
 close all
 
@@ -13,58 +16,63 @@ y0 = [1 0 0 0]';
 
 % input velocity
 x = zeros(size(trange));
-x(trange > 1 & trange <2) = 1;
+x(trange > 1 & trange <2) = 1; % angular velocity around x
 y = zeros(size(trange));
-y(trange > 3 & trange <4) = 2;
+y(trange > 3 & trange <4) = 2; % angular velocity around y
 z = zeros(size(trange));
-z(trange > 5 & trange <9) = -1;
-
+z(trange > 5 & trange <9) = -1; % angular velocity around y
 x =[x y z];
 
 % so3 attractor
-params.Aq  = eye(5);
-params.Aq(end,end)=-1;
-% params.FP = 0;
+params.Aq = eye(5);
+params.Aq(end,end)=-1; % conic section extended to be the hypersphere containing SO3
 
 
 [t, e] = ode45(@(t,y)ode2DAttractor(t,y,trange,x, params), trange, y0);
 
 figure
 subplot(3,3,1);
-plot(e(:,1),e(:,2)); set(gca,'xlim',[-1 3],'ylim',[-1 3])
-xlabel( 'Internal unit 1'), ylabel( 'Internal unit 2')
-title('Ring attractor')
+plot(e(:,1),e(:,2)); set(gca,'xlim',[-1 1]*1.2,'ylim',[-1 1]*1.2)
+xlabel( 'unit 1'), ylabel( 'unit 2')
+title('SO3 attractor')
 set(gca,'PlotBoxAspectRatio',[1 1 1])
+
+subplot(3,3,4);
+plot(e(:,2),e(:,3)); set(gca,'xlim',[-1 1]*1.2,'ylim',[-1 1]*1.2)
+xlabel( 'unit 2'), ylabel( 'unit 3')
+set(gca,'PlotBoxAspectRatio',[1 1 1])
+
+subplot(3,3,7);
+plot(e(:,3),e(:,4)); set(gca,'xlim',[-1 1]*1.2,'ylim',[-1 1]*1.2)
+xlabel( 'unit 3'), ylabel( 'unit 4')
+set(gca,'PlotBoxAspectRatio',[1 1 1])
+
+
 subplot(3,3,[2 3]);
-plot(t,[x e]);
-legend({'Input velocity', 'Internal unit 1', 'Internal unit 2'})
+plot(t,x,'linewidth',2);hold
+plot(t,e);
+legend({'Input velocity x', 'Input velocity y', 'Input velocity z', 'unit 1', 'unit 2', 'unit 3', 'unit 4'})
 xlabel('Time')
-subplot(3,3,[4:9]);
+subplot(3,3,[5 6 8 9]);
 imagesc(e')
 set(gca,'clim', [min(min(e(trange>1,:))), max(max(e(trange>1,:)))]) % make the clim ignore the first 100 timepoints
+xlabel('Time')
+ylabel('units')
+set(gca,'xticklabel',[],'yticklabel',[])
 
 
 function yd = ode2DAttractor(t,y,xt,x,params)
     x = interp1(xt,x,t); % Interpolate the data set (gt,g) at time t
     
-    if ( t>1.5)
-        a=1;
-    end
     y = [y;1]; % make it homogeneous
 
     % x is the input
     % y is the state/output of the atractor
 
     W = params.Aq;
-    Ortho1 = [0 -1 0 0 0;1 0 0 0 0; 0 0 0 -1 0; 0 0 1 0 0; 0 0 0 0 0]; %rotates Wy to get the orthogonal direction along the attractor
+    Ortho1 = [0 -1 0 0 0;1 0 0 0 0; 0 0 0 -1 0; 0 0 1 0 0; 0 0 0 0 0]; %rotates Wy to get the orthogonal directions along the attractor
     Ortho2 = [0 0 -1 0 0;0 0 0 1 0;1 0 0 0 0; 0 -1 0 0 0; 0 0 0 0 0]; 
     Ortho3 = [0 0 0 -1 0;0 0 -1 0 0;0 1 0 0 0; 1 0 0 0 0; 0 0 0 0 0]; 
-    Gleak = 0.2; % leak speed
-
-%     fp = params.FP;
-%     leak = y(2)*fp(1)-y(1)*fp(2);
-    
-%     yd = (x-Gleak*leak)*Ortho1*W*y   -   (4*y'*W*y)*W*y;
 
     yd = x(:,1)*Ortho1*W*y +x(:,2)*Ortho2*W*y +x(:,3)*Ortho3*W*y   -   (4*y'*W*y)*W*y;
 
@@ -96,23 +104,6 @@ function yd = ode2DAttractor(t,y,xt,x,params)
     % single tensor. The tensor will define how much to movement a long
     % each of the components. 1 towards the attractor and n-1 along the
     % attractor + 1 along the input regarless of attractor
-
-end
-
-
-function [t, yout] = odeSimple(fun, trange, y0)
-if ( length(trange) == 2 )
-    trange = linspace(trange(1),trange(2),1000);
-end
-yout= zeros(length(y0),length(trange));
-t = trange;
-
-yout(:,1) = y0;
-for i=2:length(t)
-    yout(:,i) = yout(:,i-1) + fun(t(i), yout(:,i-1))*(t(i)-t(i-1)) ;
-end
-
-yout = yout';
 
 end
 
