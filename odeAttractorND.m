08bedbd88d82e3cdf4ee8ae80cc79c36d385872a
Jorge Otero-Millan <jom@berkeley.edu>
2024-03-17 00:09:19 -0700

    More cleaning up.

--- odeAttractorND.m
+++ odeAttractorND.m
@@ -1,58 +1,83 @@
 function dx = odeAttractorND( ti, xi, wi, A, T, S)
 % ODEATTRACTORND  differential equation for an attractor network of m
-% dimensions embedded in an n dimensional space with p inputs. 
+% dimensions embedded in an n dimensional space with m-1 inputs. 
 %
 %   dx = odeAttractorND( ti, xi, wi, A, T, S) 
 %
 %   outputs:
+%
 %       dx: derivative of the state x at time ti
 %
 %   inputs: 
+%
 %       ti: (1 x 1) current time ti (very useful for debugging)
+%
 %       xi: (n x 1) state at time ti 
-%       wi: (q x 1) velocity input at time ti 
+%
+%       wi: (m-1 x 1) velocity input at time ti 
+%
 %       A:  (m+1 x m+1) matrix defining the quadratic ecuation that specifis
 %           the attractor shape. The attractor is the set of points x that
 %           meet the condition x'*A*x=0
 %
 %           For example:
-%           for 1D rotations:
-%               A = [1 0 0; ...
-%                    0 1 0; ...
-%                    0 0 -1];
-%           for 2D rotations:
-%               A = [1 0 0 0; ...
-%                    0 1 0 0; ...
-%                    0 0 1 0; ...
-%                    0 0 0 -1];
-%           for 3D rotations:
-%               A = [1 0 0 0 0; ...
-%                    0 1 0 0 0; ...
-%                    0 0 1 0 0; ...
-%                    0 0 0 1 0; ...
-%                    0 0 0 0 -1];
 %
-%       T: (m+1 x m+1 x q+1) Tensor encoding a base of the skew symmetric
-%           matrices of size m. Each of the q matrices roates the direction
+%           for 1D rotations:
+%               A =   1   0   0
+%                     0   1   0
+%                     0   0  -1
+%
+%           for 2D rotations:
+%               A =   1   0   0   0 
+%                     0   1   0   0 
+%                     0   0   1   0 
+%                     0   0   0  -1  
+%
+%           for 3D rotations:
+%               A =   1   0   0   0   0 
+%                     0   1   0   0   0 
+%                     0   0   1   0   0 
+%                     0   0   0   1   0 
+%                     0   0   0   0  -1 
+%
+%       T: (m x m x m-1) Tensor containing a basis for the m+1 m+1 skew
+%           symmetric matrices of size m. Each of the q matrices roates the direction
 %           towards the attractor so we get one direction q directions
 %           along the tangent of the attractor. 
 %
 %           For example:
-%           for 1D rotations:
-%               T = cat(3, ...
-%                   [0 -1 0 ; 1 0 0 ; 0 0 0;]); % 3x3x2
 %
+%           for 1D rotations:
+%                T(:,:,1) =     0    -1
+%                               1     0
+% 
 %           for 2D rotations:
-%               T = cat(3, ...
-%                   [0 -1 0 0;  1 0 0 0;  0 0 0 0;  0 0 0 0], ...
-%                   [0 0 1 0;   0 0 0 0;  -1 0 0 0; 0 0 0 0]); % 4x4x3
+%
+%                T(:,:,1) =     0    -1     0
+%                               1     0     0
+%                               0     0     0
+% 
+%                T(:,:,2) =     0     0     1
+%                               0     0     0
+%                              -1     0     0
 %
 %           for 3D rotations:
-%               T = cat(3, ...
-%                   [0 -1 0 0 0;   1 0 0 0 0;  0 0 0 -1 0; 0 0 1 0 0;  0 0 0 0 0], ...
-%                   [0 0 -1 0 0;   0 0 0 1 0;  1 0 0 0 0;  0 -1 0 0 0; 0 0 0 0 0], ...
-%                   [0 0 0 -1 0;   0 0 -1 0 0; 0 1 0 0 0;  1 0 0 0 0;  0 0 0 0 0]); % 5x5x3
 %
+%                T(:,:,1) =     0    -1     0     0
+%                               1     0     0     0
+%                               0     0     0    -1
+%                               0     0     1     0
+% 
+%                T(:,:,2) =     0     0    -1     0
+%                               0     0     0     1
+%                               1     0     0     0
+%                               0    -1     0     0
+% 
+%                T(:,:,3) =     0     0     0    -1
+%                               0     0    -1     0
+%                               0     1     0     0
+%                               1     0     0     0
+% 
 %       S: (n x m) Matrix to project x into a subspace that contains the
 %           attractor. Each column is a base vector defining the subspace.
 %
@@ -64,12 +89,17 @@ function dx = odeAttractorND( ti, xi, wi, A, T, S)
 %
     x = [xi;1]; % make the state homogeneous
     w = wi;     % just change the name for consistency
+
     [n, m] = size(S); % dimensions space and subspace
     
-    % Calculate the projection matrix subspace S that contains the
-    % attractor. Added column and row for the homogenous component 
-    P = [(S'*S)\S' zeros(m,1); zeros(1,n) 1];
-    S = [S zeros(n,1); zeros(1,m) 1];
+    % Calculate the projection matrix for subspace S that contains the
+    % attractor
+    P = (S'*S)\S';
+    
+    % Pad the matrices to deal with the homogenous component
+    P = [ P   zeros(m,1)     ;  zeros(1,n)    1  ];
+    S = [ S   zeros(n,1)     ;  zeros(1,m)    1  ];
+    T = [ T   zeros(m,1,m-1) ;  zeros(1,m+1,m-1) ];
 
     %
     % Differential equation dx/dt = f(x, w | A, T, S)
@@ -89,5 +119,5 @@ function dx = odeAttractorND( ti, xi, wi, A, T, S)
 
     dx = S*tensorprod(T,w,3,1) * A*P*x - S*(4*x'*P'*A*P*x*A*P*x) + (S*P*x-x);
 
-    dx = dx(1:end-1); % remove the homogenous component
+    dx = dx(1:end-1); % remove the homogeneous component
 end
